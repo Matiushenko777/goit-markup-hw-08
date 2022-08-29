38fd1c3d198f4550b4273c4bf9aca1a6d715c537
Matiushenko777 <romappstore@gmail.com>
2022-08-29 17:59:42 +0300

    hm-8

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector(".menu-open"),
+      closeMenuBtn: document.querySelector(".menu-close"),
+      menu: document.querySelector(".mob-menu"),
+      body: document.querySelector("body"),
+      menulist: document.querySelector(".mob-menu-list"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleMenu);
+    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    refs.menulist.addEventListener("click", removeMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
+      refs.body.classList.toggle("no-scroll");
+    }
+    function removeMenu() {
+        refs.menu.classList.add("is-hidden");
+      }
+  })();
+  
+  
+  
